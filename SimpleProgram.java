be09b9f38a59ee07a1fc49f2c8a3be0b2cfeae9a
AlaaHT <gustalebal@student.gu.se>
2022-03-25 13:08:13 +0100

    added some bics code

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -3,5 +3,11 @@ public class sayHello {
 	public static void main (String [] args){
 		
 		System.out.println("Hello World!");
+
+		int x  = 5;
+		int y = 4;
+		int z = x +y;
+
+		System.out.println("The sum is : " + z);
 	}
 }
